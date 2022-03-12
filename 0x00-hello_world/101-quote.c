8015ea38179f1452454aa26720e360687ab42ac4
angelah1994 <wafulaaangy@gmail.com>
2022-03-12 13:07:32 -0800

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main -prints a text
+ * main -Entry point of the program
  *
- * Return: Always 1 (Success)
+ * Return: print 1 if the problem is a sucess
  */
 int main(void)
 {
-	write(2, and that piece of art isuseful\" -Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art isuseful\" -Dora korpar, 2015-10-19\n", 59);
 	return(1);
 }
