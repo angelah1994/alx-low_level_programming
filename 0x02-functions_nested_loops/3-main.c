d450810e89c48827f68bbd3969af863e85cd097b
angelah1994 <wafulaaangy@gmail.com>
2022-03-15 14:31:51 -0700

    low

---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,18 @@
+/**
+ * main -check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int r;
+
+	r = -islower('H');
+	_putchar(r + '0');
+	r = _islower9'o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
