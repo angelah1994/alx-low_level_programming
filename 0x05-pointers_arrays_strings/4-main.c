7b158c74febfce892da5d019d439f5f03c72e073
angelah1994 <wafulaaangy@gmail.com>
2022-03-21 06:00:04 -0700

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fera computers. I fera the lack of them - Isaac Asiov";
+	print_rev(str);
+		return (0);
+}
